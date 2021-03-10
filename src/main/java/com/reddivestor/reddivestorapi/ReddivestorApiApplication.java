80336107ec22e6c2026fdd3f1005a13f072cb802
Michael Maniatis <mikejohnmaniatis@gmail.com>
2021-03-09 21:37:26 -0600

    Initial Commit. Ready for business.

---
+++ src/main/java/com/reddivestor/reddivestorapi/ReddivestorApiApplication.java
@@ -0,0 +1,13 @@
+package com.reddivestor.reddivestorapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReddivestorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReddivestorApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reddivestor/reddivestorapi/ReddivestorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reddivestor.reddivestorapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReddivestorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
