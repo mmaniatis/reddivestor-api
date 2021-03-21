0292581e528c5c641745de356c2bd945081ec386
Michael Maniatis <mikejohnmaniatis@gmail.com>
2021-03-21 05:07:28 -0500

    FEATURE-5 updated config to read from yml, and set yml to read sensitive info from environment variables.

--- src/main/java/com/reddivestor/reddivestorapi/persist/spring/config/MongoConfig.java
+++ src/main/java/com/reddivestor/reddivestorapi/persist/spring/config/MongoConfig.java
@@ -3,17 +3,27 @@ package com.reddivestor.reddivestorapi.persist.spring.config;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoDatabase;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
 import java.util.Set;
 
+
+@EnableConfigurationProperties
 @Configuration
 public class MongoConfig {
     //TODO: Map these to yaml file.
+    @Value("${reddivestorapi.mongodb.username}")
     String username;
+
+    @Value("${reddivestorapi.mongodb.password}")
     String password;
+
+    @Value("${reddivestorapi.mongodb.database}")
     String database;
     @Bean
     public MongoClient mongoClient() {
